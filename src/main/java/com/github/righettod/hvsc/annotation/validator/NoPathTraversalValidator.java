faeabf86bf1447bc38b1e177eaf6ed973178d342
Dominique RIGHETTO <dominique.righetto@gmail.com>
2014-02-28 16:25:42 +0100

    Revert "Add detection of the NULL byte into the 'Path Traversal' validator."
    This reverts commit dce109041b9b9d07e5ea91da99c283ed19ca7533.

--- src/main/java/com/github/righettod/hvsc/annotation/validator/NoPathTraversalValidator.java
+++ src/main/java/com/github/righettod/hvsc/annotation/validator/NoPathTraversalValidator.java
@@ -70,16 +70,8 @@ public class NoPathTraversalValidator extends BaseValidator implements Constrain
 						break;
 					}
 				}
-				// Step 3: Manage case of the NULL Byte used to stop String expression
-				for (int i = 0; i < decodedValueLC.length(); i++) {
-					if (decodedValueLC.codePointAt(i) == 0) {
-						isValidFlg = false;
-						break;
-					}
-				}
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error("Error during data validation !", e);
 			isValidFlg = false;
 		}
